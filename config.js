16654bc988e15c721ac6c5093a380259952d193d
CalinuxVix221 <calinuxvix444@gmail.com>
2025-07-27 19:07:48 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  SESSION_ID: "drakonis~l7l3DTBZ#vU1AI-liYg-nwriZwXCNBuJ3RjZlyuvyr-gzbt_5O_k",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "50955645090", // PUT OWNER NUMBER HERE
+  PREFIX: "*" // prefix (e.g., ., /, !, *)
 };
